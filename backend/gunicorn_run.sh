422f576ce97a8c71a8f6965a8095a04b0d9c58d7
Alexander Golovnya <algol.mail@gmail.com>
2020-07-14 00:15:20 +0700

    Add docker and change app structure.

---
+++ backend/gunicorn_run.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env sh
+set -e
+
+if [ -f /app/app/main.py ]; then
+    DEFAULT_MODULE_NAME=app.main
+elif [ -f /app/main.py ]; then
+    DEFAULT_MODULE_NAME=main
+fi
+MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
+VARIABLE_NAME=${VARIABLE_NAME:-app}
+export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
+
+HOST=${HOST:-0.0.0.0}
+PORT=${PORT:-80}
+LOG_LEVEL=${LOG_LEVEL:-info}
+
+# Let the DB start
+python /app/backend_pre_start.py
+
+# Run migrations
+alembic upgrade head
+
+echo "Starting server"
+exec uvicorn --reload --host $HOST --port $PORT --log-level $LOG_LEVEL "$APP_MODULE"
+#exec gunicorn main:app -w 1 -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker
